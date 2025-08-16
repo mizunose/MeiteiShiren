c68cb1f24a8fbb3ff61a28a52a480b199a735f31
Mizunose <222115394+mizunose@users.noreply.github.com>
2025-08-16 09:29:50 +0900

    入力管理の本体(コミット漏れ)

---
+++ MeiteiShiren/Assets/Scripts/Input/IngameInputManager.cs
@@ -0,0 +1,136 @@
+﻿/*=====
+<IngameInputManager.cs>
+
+-author
+	mizunose
+
+-about
+	インゲーム画面でのプレイヤー入力管理を実装
+=====*/
+
+// クラス定義
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// <para>インゲーム画面での入力</para>
+/// </summary>
+public class IngameInputManager : MonoSingleton<IngameInputManager>
+{
+	// クラス定義
+	/// <summary>
+	/// <para>プレイヤー入力管理</para>
+	/// </summary>
+	public class IngamePlayerInput : InputMapManager
+	{
+		//	変数宣言
+		IngameInputManager _parent;	// 親クラスの実体
+
+		// プロパティ定義
+	
+		/// <summary>
+		/// <para>扱う対象のInputMap</para>
+		/// </summary>
+		/// <value>子クラスで管理するInputMap</value>
+		protected override InputActionMap Target
+		{
+			get
+			{
+
+				// 提供
+				return _parent.Target.Player;	// インスタンス本体
+			}
+		}
+
+		/// <summary>
+		/// <para>移動入力値</para>
+		/// </summary>
+		/// <value>移動入力受付インスタンス</value>
+		public InputAction Move
+		{
+			get
+			{
+				return _parent.Target.Player.Move;
+			}
+		}
+
+		/// <summary>
+		/// <para>攻撃入力値</para>
+		/// </summary>
+		/// <value>攻撃入力受付インスタンス</value>
+		public InputAction Attack
+		{
+			get
+			{
+				return _parent.Target.Player.Attack;
+			}
+		}
+
+
+		/// <summary>
+		/// <para>初期化処理</para>
+		/// </summary>
+		/// <param name="parent">親クラス</param>
+		public IngamePlayerInput(in IngameInputManager parent)
+		{
+			// 初期化
+			_parent = parent;	// 親クラスのインスタンスを登録
+		}
+	}
+
+	// 変数宣言
+	private IngameInput _target;	// 管理対象マップを所持するインスタンス
+	private IngamePlayerInput _player;	// プレイヤー入力の管理インスタンス
+
+	// プロパティ定義
+
+	/// <summary>
+	/// <para>InputMap</para>
+	/// </summary>
+	/// <value>子クラスで管理するInputMap</value>
+	private IngameInput Target
+	{
+		get
+		{
+			// 保全
+			if ( _target  == null)	// ヌルチェック
+			{
+				_target = new();	// 作成
+			}
+			
+			// 提供
+			return _target;	// インスタンス本体
+		}
+	}
+
+	/// <summary>
+	/// <para>プレイヤー入力管理</para>
+	/// </summary>
+	/// <value></value>
+	public IngamePlayerInput Player
+	{
+		get
+		{
+			// 保全
+			if (_player == null)	// ヌルチェック
+			{
+				_player = new(this);	// 作成
+			}
+			
+			// 提供
+			return _player;	// インスタンス本体
+		}
+	}
+
+
+	/// <summary>
+	/// <para>破棄時処理</para>
+	/// </summary>
+	protected override void CustomOnDestroy()
+	{
+		// 解放
+		if (_target != null)	// ヌルチェック
+		{
+			_target.Dispose();	// 内部状態を解放(GC対象外)
+		}
+	}
+}
---
+++ MeiteiShiren/Assets/Scripts/Status/Attack/InputAttack.cs
@@ -0,0 +1,145 @@
+﻿/*=====
+<InputAttack.cs>
+
+-author
+	mizunose
+
+-about
+	入力攻撃を実装
+=====*/
+
+// 名前空間宣言
+using System.Collections;
+using UnityEngine;
+
+// クラス定義
+/// <summary>
+/// <para>入力攻撃</para>
+/// </summary>
+public class InputAttack : Attack
+{
+	// 定数定義
+	private const float _MOTION_SPEND = 1.0f;	// モーションの再生時間
+
+
+	/// <summary>
+	/// <para>初期化処理</para>
+	/// </summary>
+	private void Start()
+	{
+		// 更新
+		StartCoroutine(LateableUpdate());	// 更新処理の軌道
+	}
+
+
+	/// <summary>
+	/// <para>遅延可能な更新処理</para>
+	/// </summary>
+	/// <returns>遅延処理用のインターフェース</returns>
+	private IEnumerator LateableUpdate()
+	{
+		// フレーム更新
+		while (true)
+		{
+			// 入力処理
+			if (IngameInputManager.Instance.Player.Attack.IsPressed())	// 攻撃入力中
+			{
+				yield return AttackMotion();	// 攻撃モーションを実行 (モーション完了待機)
+			}
+
+			// 待機
+			yield return null;	// 次フレームを待つ
+		}
+	}
+
+
+	/// <summary>
+	/// <para>攻撃モーション処理</para>
+	/// </summary>
+	/// <returns>遅延処理用のインターフェース</returns>
+	protected override IEnumerator AttackMotion()
+	{
+		// 入力管理
+		IngameInputManager.Instance.Player.TrendDisable();	// 入力系の処理なのでモーション中は干渉権を無効化する(有効化はターン側の一存に委ねる)
+		
+		//TODO:モーション再生
+		//if (PlayableGraph _playable_graph.IsPlaying) { yield return null; }	// モーション中は待機
+
+		// 代替処理
+			// 変数宣言
+			Vector3 _at = transform.forward.normalized;	// 到達地点
+			Vector3 _from = Vector3.zero;	// 出発地点
+
+			// 変数宣言
+			float _time = 0.0f;	// 経過時間
+
+			// 前隙モーションを取る
+			while (true)	// フレーム単位でのループ
+			{
+				// 更新
+				_time += Time.deltaTime;	// 経過時間を測定
+
+				// 変数宣言
+				float _timerate = _time / (_MOTION_SPEND * 0.5f);	// 経過時間の割合
+
+				// 補正
+				if(_timerate > 1.0f)	// 時間経過が過剰
+				{
+					_timerate = 1.0f;	// 割合を丸め込む
+				}
+
+				// 攻撃
+				transform.localPosition = Vector3.Lerp(_from, _at,  - (Mathf.Cos(Mathf.PI * _timerate) - 1.0f ) /2.0f);	// イージング攻撃
+
+				// 終了
+				if (_timerate == 1.0f)	// モーション完了
+				{
+					break;	// 処理完了
+				}
+				else
+				{
+					// 待機
+					yield return null;	// 次フレームを待つ
+				}
+			}
+
+			// 内容処理
+			OnAttacked(transform.eulerAngles.y);	// 攻撃の効果発生タイミング
+
+			// 初期化
+			_time = 0.0f;	// 経過時間
+
+			// 後隙モーションを取る
+			while (true)	// フレーム単位でのループ
+			{
+				// 更新
+				_time += Time.deltaTime;	// 経過時間を測定
+
+				// 変数宣言
+				float _timerate = _time / (_MOTION_SPEND * 0.5f);	// 経過時間の割合
+
+				// 補正
+				if(_timerate > 1.0f)	// 時間経過が過剰
+				{
+					_timerate = 1.0f;	// 割合を丸め込む
+				}
+
+				// 攻撃
+				transform.localPosition = Vector3.Lerp(_at, _from,  - (Mathf.Cos(Mathf.PI * _timerate) - 1.0f ) /2.0f);	// イージング攻撃
+
+				// 終了
+				if (_timerate == 1.0f)	// モーション完了
+				{
+					break;	// 処理完了
+				}
+				else
+				{
+					// 待機
+					yield return null;	// 次フレームを待つ
+				}
+			}
+
+		// 入力管理
+		IngameInputManager.Instance.Player.TrendEnable();	// プレイヤーの干渉権を復権させる
+	}
+}
